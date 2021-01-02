00edc13a20f1c208ff56de10b779c0c45f681a0a
ricky8888 <whattodo2014@163.com>
2021-01-02 20:47:47 +0800

    modify project structure and add handler module

---
+++ springme-handler/src/main/java/com/ron/handler/SpringmeHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.ron.handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmeHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmeHandlerApplication.class, args);
+	}
+
+}
---
+++ springme-handler/src/test/java/com/ron/handler/SpringmeHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ron.handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmeHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
